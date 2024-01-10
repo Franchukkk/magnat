cd4674bbb5fd68a69329d6ab3f2c51f8e2aecb48
Franchukkk <sha471251@gmail.com>
2024-01-10 20:12:02 +0200

    carousel

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // вивожу у консоль id вибраного товару
             const buyBtns = document.querySelectorAll(".cta-card")
 
-            console.log(buyBtns);
+            console.log(buyBtns)
 
             buyBtns.forEach(function (e) {
                 e.addEventListener("click", function (i) {
@@ -29,9 +29,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     fetch('products.json')
                         .then(response => response.json())
                         .then(products => {
-                            const product = products.find(product => product.id === productID);
-                            console.log(product);
-                        });
+                            const product = products.find(product => product.id === productID)
+                            console.log(product)
+                        })
                 })
             })
         })
@@ -107,5 +107,73 @@ document.addEventListener("DOMContentLoaded", function () {
             // Додати елемент до списку
             productList.appendChild(listItem)
         })
+    }
+
+    const container = document.querySelector(".header-carousel"),
+        seasonWinter = document.querySelector(".winter"),
+        seasonSummer = document.querySelector(".summer"),
+        arrLeft = document.querySelector(".arr-left"),
+        arrRight = document.querySelector(".arr-right")
+    let currentFrame = 0,
+        backgroundCarousel
+
+    function animateBackground() {
+        if (currentFrame === 0 || currentFrame === 100) {
+            container.style.backgroundImage = 'url(../img/header-bg-1.jpg)'
+            updateSeasonDisplay(seasonSummer, 0, "none", "0")
+            updateSeasonDisplay(seasonWinter, 1, "inline", "1")
+        } else if (currentFrame === 50) {
+            container.style.backgroundImage = 'url(../img/header-background-2.jpg)'
+            updateSeasonDisplay(seasonWinter, 0, "none", "0")
+            updateSeasonDisplay(seasonSummer, 1, "inline", "1")
+        }
+
+        currentFrame = (currentFrame + 1) % 101
+    }
+
+    function updateSeasonDisplay(season, opacityStart, displayValue, opacityEnd) {
+        season.style.opacity = opacityStart
+        season.style.display = displayValue
+        let opacityInterval = setInterval(() => {
+            season.style.opacity = opacityEnd
+            clearInterval(opacityInterval)
+        }, 0)
+    }
+
+    function startAnimation() {
+        animateBackground()
+        backgroundCarousel = setInterval(animateBackground, 100)
+    }
+
+    startAnimation()
+
+    arrLeft.addEventListener("click", changeSlideArr)
+    arrRight.addEventListener("click", changeSlideArr)
+    let canClick = true
+
+    function changeSlideArr() {
+        if (!canClick) {
+            return 
+        } else {
+            canClick = false
+            clearInterval(backgroundCarousel)
+            if (seasonWinter.style.opacity == "1") {
+                container.style.backgroundImage = 'url(../img/header-background-2.jpg)'
+                updateSeasonDisplay(seasonWinter, 0, "none", "0")
+                updateSeasonDisplay(seasonSummer, 1, "inline", "1")
+            } else {
+                console.log(2)
+                container.style.backgroundImage = 'url(../img/header-bg-1.jpg)'
+                updateSeasonDisplay(seasonSummer, 0, "none", "0")
+                updateSeasonDisplay(seasonWinter, 1, "inline", "1")
+            }
+
+            let arrowDelay = setInterval(() => {
+                canClick = true
+                clearInterval(arrowDelay)
+            }, 1000)
+
+            startAnimation()
+        }
     }
 })
