8e6e5367ec4eeb2d3e3abbedd260601b4782c2bc
Zavorotna <zavorotnaa96@gmail.com>
2024-01-11 23:20:51 +0200

    inputs range

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -166,8 +166,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 inputBlock.appendChild(labelInput);
             }
 
-
-
             // створити кнопку
             const ctaBuy = document.createElement("a")
             ctaBuy.classList.add("cta-card")
@@ -250,6 +248,7 @@ document.addEventListener("DOMContentLoaded", function () {
             startAnimation()
         }
     }
+
     const cart = document.querySelector(".cart"),
     basketPopup = document.querySelector(".basket-popup"),
     returnBasket = document.querySelector(".basket-back")
@@ -260,4 +259,48 @@ document.addEventListener("DOMContentLoaded", function () {
 
     cart.addEventListener("click", basketToggle)
     returnBasket.addEventListener("click", basketToggle)
+
+    //input range 
+    const rangeInput = document.querySelectorAll(".range-input input"),
+    priceInput = document.querySelectorAll(".price-input input"),
+    range = document.querySelector(".slider .progress");
+    let priceGap = 1000;
+
+    priceInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+
+        if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+        if (e.target.className === "input-min") {
+            rangeInput[0].value = minPrice;
+            range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+        } else {
+            rangeInput[1].value = maxPrice;
+            range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+        }
+        }
+    });
+    });
+
+    rangeInput.forEach((input) => {
+    input.addEventListener("input", (e) => {
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+
+        if (maxVal - minVal < priceGap) {
+        if (e.target.className === "range-min") {
+            rangeInput[0].value = maxVal - priceGap;
+        } else {
+            rangeInput[1].value = minVal + priceGap;
+        }
+        } else {
+        priceInput[0].value = minVal;
+        priceInput[1].value = maxVal;
+        range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+        range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+    });
+
 })
